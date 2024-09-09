0d04ee3b78cc0560b9a56dfe6fbebdb699d1a051
minhnx8 <minhnx.hust>
2024-09-09 22:38:07 +0700

    đá

--- delete.sh
+++ delete.sh
@@ -1,3 +1,2 @@
-# script to delete a CloudFormation Stack.
 aws cloudformation delete-stack \
 --stack-name $1
--- starter/create.sh
+++
@@ -1,6 +0,0 @@
-# script to create a CloudFormation Stack.
-stack_name=$1
-template_file=$2
-parameters_file=$3
-shift 3
-aws cloudformation create-stack --stack-name $stack_name --template-body file://$template_file --parameters file://$parameters_file "$@" --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
